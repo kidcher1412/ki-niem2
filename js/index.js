d4bc96272c579f4ccf78adee9c886788363d5183
kidcher1412 <kidcher1412@gmail.com>
2022-07-01 06:53:28 +0700

    the last change from Me

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+$("#login-button").click(function (event) {
+    var userName = document.getElementById("userName").value;
+    var pwd = document.getElementById("pwd").value;
+    if (userName == "thong" && pwd == "30062002" || userName == "thong" && pwd == "11122001" || userName == "chau" && pwd == "11112001" || userName == "chou" && pwd == "11112001") {
+      event.preventDefault();
+      $("form").fadeOut(500);
+      $(".wrapper").addClass("form-success");
+      setTimeout(function () {
+        location.href = "Cake.html";
+      }, 2000);
+    } else {
+      alert("Wrong Password");
+    }
+  });
