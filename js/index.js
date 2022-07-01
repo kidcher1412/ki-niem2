6411b8cbe2175e502b872ad78f03ba6311765be0
THONG <100288144+kidcher1412@users.noreply.github.com>
2022-07-01 20:11:21 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 $("#login-button").click(function (event) {
     var userName = document.getElementById("userName").value;
     var pwd = document.getElementById("pwd").value;
-    if (userName == "thong" && pwd == "30062002" || userName == "thong" && pwd == "11122001" || userName == "chau" && pwd == "11112001" || userName == "chou" && pwd == "11112001") {
+    if (userName == "thong" && pwd == "30062002" || userName == "HoangLeAnhThong" && pwd == "30062002" || userName == "HoangLeAnhThong" && pwd == "11122001" || userName == "thong" && pwd == "11122001" || userName == "chau" && pwd == "11112001" || userName == "chou" && pwd == "11112001" || userName == "LeTranQuynhChau" && pwd == "11112001") {
       event.preventDefault();
       $("form").fadeOut(500);
       $(".wrapper").addClass("form-success");
